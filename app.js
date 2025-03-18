1c3921be9f0ff773622136ecd0bcfc09769ce230
jabalma <jabalma@gmail.com>
2025-03-18 18:04:23 -0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos = [];
+const resultado = document.getElementById('resultado');
+const agregar = document.querySelector('.button-add');
+const elegir = document.querySelector('.button-draw');
+const nombre = document.querySelector('.input-name');
